e5635395a36829c89967e6be5d655210a14ae6ac
Junbang <biatbang@163.com>
2019-11-18 10:19:17 -0500

    front-end of schedule page

--- app/schedule.py
+++ app/schedule.py
@@ -1,9 +1,30 @@
-from flask import render_template
+from flask import render_template, request
 from app import webapp
+from app.utils import awsUtils
+
+awsSuite = awsUtils.AWSSuite()
 
 """
 redirect to this page, retrieve all items in cart.
 """
 @webapp.route('/viewCart')
-def viewCart():
-    return render_template('schedule.html')
+def viewCart():    
+    # for now, after userId should come from 
+    userId = "qwertyuiopoi"   
+    userItem = awsSuite.getUserById(userId)
+    cartItems = userItem['cart']
+    spots = []
+    for cartItem in cartItems:
+        print(cartItem)
+        spots.append(awsSuite.getSpotById(cartItem))
+    print(spots)
+
+    return render_template('schedule.html', userItem=userItem, spots=spots)
+
+@webapp.route('/addSpotToSchedule', methods=['POST'])
+def addSpotToSchedule():
+    date = request.form['date']
+    startTime = request.form['startTime']
+    endTime = request.form['endTime']
+    print(date, startTime, endTime)
+    return ('', 204)
---
+++ app/utils/awsUtils.py
@@ -0,0 +1,24 @@
+import boto3
+
+
+class AWSSuite():
+    def __init__(self):
+        self.dynamodb = boto3.resource('dynamodb')
+
+    def getUserById(self, userId):
+        userTable = self.dynamodb.Table('user')
+        response = userTable.get_item(Key={'userId': userId})
+        if 'Item' in response:
+            userItem = response['Item']
+        else:
+            userItem = None
+        return userItem
+
+    def getSpotById(self, spotId):
+        spotTable = self.dynamodb.Table('spot')
+        response = spotTable.get_item(Key={'spotId': spotId})
+        if 'Item' in response:
+            spotItem = response['Item']
+        else:
+            spotItem = None
+        return spotItem
